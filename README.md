f87847afa39d6314047d072b72610b07dedd9f4a
Danilo Santos <danilopsnts@gmail.com>
2022-08-09 08:35:07 -0300

    add validacoes e readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mob-25-aplicacoes-android-aula3-gasolina
-Cálculo Gasolina ou Alcool
+
+# Gasolina ou álcool?
+VersionCode: 1.0
+
+VersionName: "1"
+
+# Introdução
+Calculando se vale a pena colocar Gasolina ou Álcool
+
+![Aplicativo 1](img_readme/v1.png)
+
+
+# Authors
+
+Danilo Santos
+[@danilopsnts](https://www.linkedin.com/in/danilopsnts/)
--- app/src/main/java/com/example/exercicio2/MainActivity.kt
+++ app/src/main/java/com/example/exercicio2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.exercicio2
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
+        * add um botão para limpar os campos
+        * caso queira colocar outros valores
+        *
+        * */
+
+        btn_limpar.setOnClickListener {
+            txt_resultado_gasolina_alcool.visibility = TextView.INVISIBLE
+            edit_txt_preco_gasolina.setText("")
+            edit_txt_preco_alcool.setText("")
+        }
+
         btn_calcular.setOnClickListener {
 
             val valorDigitadoGasolina = edit_txt_preco_gasolina.text.toString()
@@ -21,17 +34,24 @@ class MainActivity : AppCompatActivity() {
             if (validarCampos) {
                 calculaMelhorPreco(valorDigitadoAlcool, valorDigitadoGasolina)
             } else {
+                txt_resultado_gasolina_alcool.visibility = TextView.VISIBLE
                 txt_resultado_gasolina_alcool.text = "Preenche os campos primeiros"
             }
         }
     }
 
+    /*
+    * add hide and show, caso o cliente queira preencher outros valores
+    * */
+
     @SuppressLint("SetTextI18n")
     private fun calculaMelhorPreco(precoAlcool: String, precoGasolina: String) {
         val resultado = precoAlcool.toDouble() / precoGasolina.toDouble()
         if (resultado >= 0.7) {
+            txt_resultado_gasolina_alcool.visibility = TextView.VISIBLE
             txt_resultado_gasolina_alcool.text = "Melhor utilizar Gasolina"
         } else {
+            txt_resultado_gasolina_alcool.visibility = TextView.VISIBLE
             txt_resultado_gasolina_alcool.text = "Melhor utilizar Alcool"
         }
     }
